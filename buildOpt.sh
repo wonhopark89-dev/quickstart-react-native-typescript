3e562fcd7abbbf46cfe080f0d876a8da129b6044
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-09-26 15:31:04 +0900

    set Config

---
+++ buildOpt.sh
@@ -0,0 +1,7 @@
+echo "Ready ........................."
+echo "adb port clear ...."
+adb reverse --remove-all
+echo "adb port 8081 to 8081 ...."
+adb reverse tcp:8081 tcp:8081
+echo "Start react-native Metro ......."
+npm start -- --reset-cache
---
+++ src/RootScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const RootScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>RootScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default RootScreen;
