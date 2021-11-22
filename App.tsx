a10c813c4d7ef32ca1055504901f22db2c2d2162
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-11-23 00:30:44 +0900

    ᕦ(ò_óˇ)ᕤ Fix XCode Build Option about Swift and Hermes engine version Issue

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import 'react-native-gesture-handler';
+import 'react-native-reanimated';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import RootStackNavigator from '~/navigators/RootStackNavigator';
