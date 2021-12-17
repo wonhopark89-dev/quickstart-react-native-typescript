8014b1f0397bed78f4cf6f811322ac0a1f4477eb
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-12-17 23:57:20 +0900

    Change preloading to hook

--- movies-app/App.js
+++ movies-app/App.js
@@ -3,32 +3,14 @@ import React, { useState } from 'react';
 import * as Font from 'expo-font';
 import { Text, Image } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import { Asset } from 'expo-asset';
-
-const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
-const loadImages = (images) =>
-  images.map((image) => {
-    if (typeof image === 'string') {
-      return Image.prefetch(image);
-    } else {
-      return Asset.loadAsync(image);
-    }
-  });
+import { Asset, useAssets } from 'expo-asset';
 
 export default function App() {
-  const [ready, setReady] = useState(false);
+  const [assets] = useAssets([require('./oss_logo.png')]);
+  const [loaded, error] = Font.useFonts(Ionicons.font);
 
-  const onFinish = () => setReady(true);
-  const startLoading = async () => {
-    const fonts = loadFonts([Ionicons.font]);
-    const images = loadImages([
-      require('./oss_logo.png'),
-      'https://d33wubrfki0l68.cloudfront.net/01eeacc4cb1ecbbd986af3acbec3addd264170c7/2673a/img/showcase/qq.png',
-    ]);
-  };
-
-  if (!ready) {
-    return <AppLoading startAsync={startLoading} onFinish={onFinish} onError={console.error} />;
+  if (!assets || !loaded) {
+    return <AppLoading />;
   }
 
   return <Text>We are done</Text>;
