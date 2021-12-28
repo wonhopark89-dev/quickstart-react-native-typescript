87e30f3434453f8bbb93aa24128a6029b3296adf
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-12-29 02:55:07 +0900

    styled-components

--- movies-app/screens/Movies.tsx
+++ movies-app/screens/Movies.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components/native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import Swiper from 'react-native-swiper';
 import Slide from '../components/Slide';
+import Poster from '../components/Poster';
 const API_KEY = '99c145303e44f10255f3c26a1a745124';
 
 // https://api.themoviedb.org/3/movie/now_playing?api_key=99c145303e44f10255f3c26a1a745124&language=en-US&page=1&region=KR
@@ -20,7 +21,60 @@ const Loader = styled.View`
   align-items: center;
 `;
 
-// https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg
+const ListTitle = styled.Text`
+  color: ${(props) => props.theme.textColor};
+  font-size: 18px;
+  font-weight: 600;
+  margin-left: 30px;
+`;
+
+const TrendingScroll = styled.ScrollView`
+  margin-top: 20px;
+`;
+
+const Movie = styled.View`
+  margin-right: 20px;
+  align-items: center;
+`;
+
+const Title = styled.Text`
+  color: ${(props) => props.theme.textColor};
+  font-weight: 600;
+  margin-top: 6px;
+  margin-bottom: 4px;
+`;
+
+const ListContainer = styled.View`
+  margin-bottom: 30px;
+`;
+
+const Votes = styled.Text`
+  color: ${(props) => props.theme.textColor};
+  font-size: 10px;
+`;
+
+const HMovie = styled.View`
+  padding: 0px 30px;
+  margin-bottom: 20px;
+  flex-direction: row;
+`;
+const HColumn = styled.View`
+  margin-left: 15px;
+  width: 80%;
+`;
+
+const Overview = styled.Text`
+  color: ${(props) => props.theme.textColor};
+  opacity: 0.9;
+  width: 80%;
+`;
+
+const Release = styled.Text`
+  font-size: 12px;
+  color: ${(props) => props.theme.textColor};
+  margin-top: 8px;
+  margin-bottom: 8px;
+`;
 
 const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = ({ navigation: { navigate } }) => {
   const isDark = useColorScheme() === 'dark';
@@ -76,7 +130,7 @@ const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = ({ navigation: {
         autoplay
         autoplayTimeout={3.5}
         showsPagination={false}
-        containerStyle={{ width: '100%', height: SCREEN_HEIGHT / 4 }}>
+        containerStyle={{ marginBottom: 30, width: '100%', height: SCREEN_HEIGHT / 4 }}>
         {nowPlaying.map((movie, index) => (
           <Slide
             key={movie.id}
@@ -88,6 +142,43 @@ const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = ({ navigation: {
           />
         ))}
       </Swiper>
+      <ListContainer>
+        <ListTitle isDark={isDark}>Trending Movies</ListTitle>
+        <TrendingScroll horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={{ paddingLeft: 20 }}>
+          {trending.map((movie) => (
+            <Movie key={movie.id}>
+              <Poster path={movie.poster_path} />
+              <Title>
+                {movie.original_title.slice(0, 13)}
+                {movie.original_title.length > 13 ? '...' : null}
+              </Title>
+              <Votes>{movie.vote_average > 0 ? `⭐️ ${movie.vote_average}/10` : `Coming soon`}</Votes>
+            </Movie>
+          ))}
+        </TrendingScroll>
+      </ListContainer>
+      <ListTitle>Coming Soon</ListTitle>
+      {upcoming.map((movie) => (
+        <HMovie key={movie.id}>
+          <Poster path={movie.poster_path} />
+          <HColumn>
+            <Title>{movie.original_title}</Title>
+            <Release>
+              {' '}
+              {new Date(movie.release_date).toLocaleDateString('ko', {
+                month: 'long',
+                day: 'numeric',
+                year: 'numeric',
+              })}
+            </Release>
+            <Overview>
+              {movie.overview !== '' && movie.overview.length > 80
+                ? `${movie.overview.slice(0, 140)}...`
+                : movie.overview}
+            </Overview>
+          </HColumn>
+        </HMovie>
+      ))}
     </Container>
   );
 };
