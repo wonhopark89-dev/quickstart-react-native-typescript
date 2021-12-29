ebe2fe48baf5cfde91ebb9e5ef7dba168130db9a
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-12-30 02:00:38 +0900

    separate component

--- movies-app/screens/Movies.tsx
+++ movies-app/screens/Movies.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, Dimensions, RefreshControl, useColorScheme } from 'react-native';
+import { ActivityIndicator, Dimensions, FlatList, RefreshControl, useColorScheme } from 'react-native';
 import styled from 'styled-components/native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import Swiper from 'react-native-swiper';
 import Slide from '../components/Slide';
-import Poster from '../components/Poster';
+import VMedia from '../components/VMedia';
+import HMedia from '../components/HMedia';
 const API_KEY = '99c145303e44f10255f3c26a1a745124';
 
 // https://api.themoviedb.org/3/movie/now_playing?api_key=99c145303e44f10255f3c26a1a745124&language=en-US&page=1&region=KR
@@ -32,48 +33,8 @@ const TrendingScroll = styled.ScrollView`
   margin-top: 20px;
 `;
 
-const Movie = styled.View`
-  margin-right: 20px;
-  align-items: center;
-`;
-
-const Title = styled.Text`
-  color: ${(props) => props.theme.textColor};
-  font-weight: 600;
-  margin-top: 6px;
-  margin-bottom: 4px;
-`;
-
 const ListContainer = styled.View`
   margin-bottom: 30px;
-`;
-
-const Votes = styled.Text`
-  color: ${(props) => props.theme.textColor};
-  font-size: 10px;
-`;
-
-const HMovie = styled.View`
-  padding: 0px 30px;
-  margin-bottom: 20px;
-  flex-direction: row;
-`;
-const HColumn = styled.View`
-  margin-left: 15px;
-  width: 80%;
-`;
-
-const Overview = styled.Text`
-  color: ${(props) => props.theme.textColor};
-  opacity: 0.9;
-  width: 80%;
-`;
-
-const Release = styled.Text`
-  font-size: 12px;
-  color: ${(props) => props.theme.textColor};
-  margin-top: 8px;
-  margin-bottom: 8px;
 `;
 
 const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = ({ navigation: { navigate } }) => {
@@ -150,41 +111,27 @@ const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = ({ navigation: {
         ))}
       </Swiper>
       <ListContainer>
-        <ListTitle isDark={isDark}>Trending Movies</ListTitle>
+        <ListTitle>Trending Movies</ListTitle>
         <TrendingScroll horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={{ paddingLeft: 20 }}>
           {trending.map((movie) => (
-            <Movie key={movie.id}>
-              <Poster path={movie.poster_path} />
-              <Title>
-                {movie.original_title.slice(0, 13)}
-                {movie.original_title.length > 13 ? '...' : null}
-              </Title>
-              <Votes>{movie.vote_average > 0 ? `⭐️ ${movie.vote_average}/10` : `Coming soon`}</Votes>
-            </Movie>
+            <VMedia
+              key={movie.id}
+              posterPath={movie.poster_path}
+              originalTitle={movie.original_title}
+              voteAverage={movie.vote_average}
+            />
           ))}
         </TrendingScroll>
       </ListContainer>
       <ListTitle>Coming Soon</ListTitle>
       {upcoming.map((movie) => (
-        <HMovie key={movie.id}>
-          <Poster path={movie.poster_path} />
-          <HColumn>
-            <Title>{movie.original_title}</Title>
-            <Release>
-              {' '}
-              {new Date(movie.release_date).toLocaleDateString('ko', {
-                month: 'long',
-                day: 'numeric',
-                year: 'numeric',
-              })}
-            </Release>
-            <Overview>
-              {movie.overview !== '' && movie.overview.length > 80
-                ? `${movie.overview.slice(0, 140)}...`
-                : movie.overview}
-            </Overview>
-          </HColumn>
-        </HMovie>
+        <HMedia
+          key={movie.id}
+          posterPath={movie.poster_path}
+          originalTitle={movie.original_title}
+          overview={movie.overview}
+          releaseDate={movie.release_date}
+        />
       ))}
     </Container>
   );
