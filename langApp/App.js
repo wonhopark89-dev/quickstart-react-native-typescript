7dd0acf3bdba2e26031683779b0ed9e4ba5e4e38
wonhopark89-dev <wonhopark89.dev@gmail.com>
2022-01-07 02:10:48 +0900

    Make Animation Component

--- langApp/App.js
+++ langApp/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components/native";
+import { Animated, TouchableOpacity } from "react-native";
 
 const Container = styled.View`
   flex: 1;
@@ -12,6 +13,16 @@ const Button = styled.TouchableOpacity`
   width: 100px;
   height: 100px;
 `;
+
+// Animated 컴포넌트 만드는 방법(1) with styled-components
+const Box1 = styled(Animated.createAnimatedComponent(TouchableOpacity))`
+  background-color: tomato;
+  width: 50px;
+  height: 50px;
+`;
+
+// Animated 컴포넌트 만드는 방법(2) with styled-components
+const AnimatedButton = Animated.createAnimatedComponent(Button);
 
 export default function App() {
   const [y, setY] = useState(0);
