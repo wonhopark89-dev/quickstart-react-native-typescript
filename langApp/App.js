452b417cc36eb71462c8bfb2381bcd6036552941
wonhopark89-dev <wonhopark89.dev@gmail.com>
2022-01-09 19:56:04 +0900

    Animation, parallel

--- langApp/App.js
+++ langApp/App.js
@@ -1,105 +1,76 @@
-import React, { useRef, useState } from "react";
+import React, { useRef } from "react";
 import styled from "styled-components/native";
-import {
-  Animated,
-  Dimensions,
-  Easing,
-  PanResponder,
-  Pressable,
-  TouchableOpacity,
-} from "react-native";
+import { View, Animated, PanResponder } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
+  background-color: #00a8ff;
 `;
 
-const Box = styled.View`
-  background-color: tomato;
-  width: 100px;
-  height: 100px;
+const Card = styled(Animated.createAnimatedComponent(View))`
+  background-color: white;
+  width: 300px;
+  height: 300px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 15px;
+  box-shadow: 1px 1px 10px rgba(255, 0, 0, 0.6);
 `;
-
-// Animated 컴포넌트 만드는 방법(1) with styled-components
-const Box1 = styled(Animated.createAnimatedComponent(TouchableOpacity))`
-  background-color: tomato;
-  width: 50px;
-  height: 50px;
-`;
-
-// Animated 컴포넌트 만드는 방법(2) with styled-components
-const AnimatedBox = Animated.createAnimatedComponent(Box);
-
-const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 export default function App() {
-  const POSITION = useRef(
-    new Animated.ValueXY({
-      x: 0,
-      y: 0,
-    })
-  ).current;
+  const position = useRef(new Animated.Value(0)).current;
+  const scale = useRef(new Animated.Value(1)).current; // 1 = 100%
+
+  const onPressIn = () =>
+    Animated.spring(scale, { toValue: 0.9, useNativeDriver: true }).start();
+  const onPressOut = Animated.spring(scale, {
+    toValue: 1,
+    useNativeDriver: true,
+  });
 
   const panResponder = useRef(
     PanResponder.create({
       onStartShouldSetPanResponder: () => true,
-      onPanResponderMove: (e, { dx, dy }) => {
-        console.log("Finger Moving");
-        POSITION.setValue({
-          x: dx,
-          y: dy,
-        });
+      onPanResponderMove: (_, { dx }) => {
+        position.setValue(dx);
       },
-      onPanResponderGrant: () => {
-        console.log("Touch Started");
-        POSITION.setOffset({
-          // 시작을 현재 위치에서  ( 0,0 이 아닌ㄷ )
-          x: POSITION.x._value,
-          y: POSITION.y._value,
-        });
-      },
+      onPanResponderGrant: () => onPressIn(),
       onPanResponderRelease: () => {
-        console.log("Touch Finished");
-        POSITION.flattenOffset();
-        // POSITION.setValue({ x: 0, y: 0 }); // 애니메이션 없이 바로 이동해버림
-        // Animated.spring(POSITION, {
-        //   toValue: { x: 0, y: 0 },
-        //   useNativeDriver: false,
-        // }).start();
+        Animated.parallel([
+          onPressOut,
+          Animated.spring(position, {
+            toValue: 0,
+            useNativeDriver: true,
+          }),
+        ]).start();
       },
     })
   ).current;
 
-  const opacityValue = POSITION.y.interpolate({
-    inputRange: [-200, 0, 200],
-    outputRange: [1, 0.5, 1],
-  });
-
-  const borderRadius = POSITION.y.interpolate({
-    inputRange: [-200, 200],
-    outputRange: [50, 0],
-  });
-
-  const backgroundColor = POSITION.y.interpolate({
-    inputRange: [-200, 200],
-    outputRange: ["rgb(255,0,255)", "rgb(0,0,255)"],
-  });
-
   return (
     <Container>
-      <AnimatedBox
+      <Card
         {...panResponder.panHandlers}
-        style={{
-          backgroundColor,
-          borderRadius,
-          opacity: opacityValue,
-          transform: [...POSITION.getTranslateTransform()],
-        }}
-      />
+        style={{ transform: [{ scale }, { translateX: position }] }}
+      >
+        <Ionicons name={"pizza"} color={"#192a56"} size={100} />
+      </Card>
     </Container>
   );
 }
+
+// Animated 컴포넌트 만드는 방법(1) with styled-components
+// const Box1 = styled(Animated.createAnimatedComponent(TouchableOpacity))`
+//   background-color: tomato;
+//   width: 50px;
+//   height: 50px;
+// `;
+
+// Animated 컴포넌트 만드는 방법(2) with styled-components
+// const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 // TouchableOpacity => 애니메이션이 자연스럽게 먹히지 않음
 // View, ScrollView 추천
@@ -128,3 +99,9 @@ export default function App() {
 //     toValue: { x: 0, y: 0 },
 //     useNativeDriver: false,
 // }).start();
+
+// POSITION.setOffset({
+//     // 시작을 현재 위치에서  ( 0,0 이 아닌ㄷ )
+//     x: POSITION.x._value,
+//     y: POSITION.y._value,
+// });
