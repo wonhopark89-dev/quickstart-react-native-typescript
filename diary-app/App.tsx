0e79c06a21d346a625a7a2a0c50cc768be857dea
wonhopark89-dev <wonhopark89.dev@gmail.com>
2022-01-17 00:07:13 +0900

    Add Realm DB

--- diary-app/App.tsx
+++ diary-app/App.tsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import Realm from 'realm';
+import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Navigator from './navigator';
+import AppLoading from 'expo-app-loading';
+
+const FeelingSchema = {
+  name: 'Feeling',
+  properties: {
+    _id: 'int',
+    emotion: 'string',
+    message: 'string',
+  },
+  primaryKey: '_id',
+};
 
 export default function App() {
+  const [ready, setReady] = useState(false);
+  const startLoading = async () => {
+    const realm = await Realm.open({
+      path: 'diaryDB',
+      schema: [FeelingSchema],
+    });
+  };
+
+  const onFinish = () => setReady(true);
+
+  if (!ready) {
+    return <AppLoading onError={console.error} onFinish={onFinish} startAsync={startLoading} />;
+  }
+
   return (
     <NavigationContainer>
       <Navigator />
---
+++ diary-app/README.md
@@ -0,0 +1,3 @@
+
+## Realm
+- https://docs.mongodb.com/realm/sdk/react-native/quick-start-local/#std-label-react-native-client-quick-start-local
