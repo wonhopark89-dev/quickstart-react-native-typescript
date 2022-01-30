e7e5d015337ac44c6e772b4199b108c4a8d6c471
wonhopark89-dev <wonhopark89.dev@gmail.com>
2022-01-31 01:30:44 +0900

    Repeat panResponder with asset

--- langApp/root.js
+++ langApp/root.js
@@ -3,13 +3,14 @@ import App from "./App";
 import DragAndDrop from "./DragAndDrop";
 import Discover from "./CoinDrop";
 import { QueryClient, QueryClientProvider } from "react-query";
+import DragAndDrop2 from "./DragAndDrop2";
 
 const client = new QueryClient();
 
 const Root = () => {
   return (
     <QueryClientProvider client={client}>
-      <Discover />
+      <DragAndDrop2 />
     </QueryClientProvider>
   );
 };
