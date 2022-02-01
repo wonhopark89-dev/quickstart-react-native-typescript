8c1c78744f8c128ca8e8d7311975bdd430d70376
wonhopark89-dev <wonhopark89.dev@gmail.com>
2022-02-01 17:47:04 +0900

    FCM 테스트 for Android (전체, 특정, 토큰)

--- NotificationApp/App.tsx
+++ NotificationApp/App.tsx
@@ -1,115 +1,102 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import React, {useEffect, useState} from 'react';
+import {Alert, SafeAreaView, Text, TouchableOpacity} from 'react-native';
+import {fcmService} from './src/push/FCMService';
+import {localNotificationService} from './src/push/LocalNotificationService';
+import messaging from '@react-native-firebase/messaging';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [tk, setTk] = useState<string>('');
+  useEffect(() => {
+    fcmService.registerAppWithFCM();
+    fcmService.register(onRegister, onNotification, onOpenNotification);
+    localNotificationService.configure(onOpenNotification);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    // @ts-ignore
+    function onRegister(token) {
+      console.log('[App] onRegister : token :', token);
+      setTk(token);
+    }
+
+    // @ts-ignore
+    function onNotification(notify) {
+      console.log('[App] onNotification : notify :', notify);
+      const options = {
+        soundName: 'default',
+        playSound: true,
+      };
+      localNotificationService.showNotification(
+        0,
+        notify.title,
+        notify.body,
+        notify,
+        options,
+      );
+    }
+
+    // @ts-ignore
+    function onOpenNotification(notify) {
+      console.log('[App] onOpenNotification : notify :', notify);
+      Alert.alert('Open Notification : notify.body :' + notify.body);
+    }
+    return () => {
+      console.log('[App] unRegister');
+      fcmService.unRegister();
+      localNotificationService.unRegister();
+    };
+  }, []);
+
+  // 구독하기
+  const callApiSubscribeTopic = (topic: string = 'Topic1') => {
+    //   return instance.post('/push');
+    return messaging()
+      .subscribeToTopic(topic)
+      .then(() => Alert.alert(`${topic} 구독 성공!!`))
+      .catch(() => Alert.alert(`${topic} 구독 실패!!`));
+  };
+
+  const callApiUnsubscribeTopic = (topic: string = 'Topic1') => {
+    //   return instance.post('/push');
+    return messaging()
+      .unsubscribeFromTopic(topic)
+      .then(() => Alert.alert(`${topic} 해제 성공!!`))
+      .catch(() => Alert.alert(`${topic} 해제 실패!!`));
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'black',
+      }}>
+      <Text style={{color: 'white'}}>Push 테스트</Text>
+      <Text style={{fontSize: 20, color: 'white'}}>{tk}</Text>
+      <TouchableOpacity
+        onPress={() => callApiSubscribeTopic()}
+        style={{
+          padding: 10,
+          alignItems: 'center',
+          borderWidth: 1,
+          borderColor: 'cyan',
+          borderRadius: 5,
+        }}>
+        <Text style={{color: 'white'}}>{`Topic1 구독하기`}</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => callApiUnsubscribeTopic()}
+        style={{
+          padding: 10,
+          alignItems: 'center',
+          borderWidth: 1,
+          borderColor: 'cyan',
+          borderRadius: 5,
+        }}>
+        <Text style={{color: 'white'}}>{`Topic1 구독해제`}</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
--- NotificationApp/index.js
+++ NotificationApp/index.js
@@ -1,9 +1,19 @@
-/**
- * @format
- */
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import messaging from '@react-native-firebase/messaging';
 
-AppRegistry.registerComponent(appName, () => App);
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('Message handleed in the background!', remoteMessage);
+});
+
+function HeadlessCheck({isHeadless}) {
+  if (isHeadless) {
+    // App has been launched in the background by IOS, ignore
+    return null;
+  }
+  return <App />;
+}
+
+AppRegistry.registerComponent(appName, () => HeadlessCheck);
