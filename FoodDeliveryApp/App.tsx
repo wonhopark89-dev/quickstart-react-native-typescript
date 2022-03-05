29da90d5927794797f050549d551c72463da528a
wonhopark89-dev <wonhopark89.dev@gmail.com>
2022-03-05 23:53:28 +0900

    Add Param to Stack Navigator

--- FoodDeliveryApp/App.tsx
+++ FoodDeliveryApp/App.tsx
@@ -39,7 +39,7 @@ function DetailsScreen({ navigation }: DetailsScreenProps) {
   );
 }
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
 function App() {
   return (
     <NavigationContainer>
