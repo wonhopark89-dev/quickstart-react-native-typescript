b0f682084794d5715c66052adc3c5e3ee8f9836e
wonhopark89-dev <wonhopark89.dev@gmail.com>
2022-03-12 23:46:37 +0900

    add package for redux

--- FoodDeliveryApp/src/components/DismissKeyboardView.tsx
+++ FoodDeliveryApp/src/components/DismissKeyboardView.tsx
@@ -1,29 +1,39 @@
 import React from 'react';
-import { TouchableWithoutFeedback, Keyboard, StyleProp, ViewStyle, KeyboardAvoidingView, Platform } from 'react-native';
+import {
+  TouchableWithoutFeedback,
+  Keyboard,
+  StyleProp,
+  ViewStyle,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+  TextInputProps,
+  TextStyle,
+} from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scrollview';
 
-// const DismissKeyboardView = ({ children, ...props }) => (
-//   <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-//     <KeyboardAwareScrollView {...props} style={props.style}>
-//       {children}
-//     </KeyboardAwareScrollView>
-//   </TouchableWithoutFeedback>
-// );
+const DismissKeyboardView: React.FC<{ style?: StyleProp<ViewStyle> }> = ({ children, ...props }) => (
+  <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+    <KeyboardAwareScrollView {...props} style={props.style}>
+      {children}
+    </KeyboardAwareScrollView>
+  </TouchableWithoutFeedback>
+);
 
 /**
  *  behavior
  *  - position : 안드로이드
  *  - padding : 아이폰
  */
-const DismissKeyboardView: React.FC<{ style?: StyleProp<ViewStyle> }> = ({ children, ...props }) => (
-  <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-    <KeyboardAvoidingView
-      {...props}
-      style={[props.style, Platform.OS === 'ios' && { flex: 1 }]}
-      behavior={Platform.OS === 'ios' ? 'padding' : 'position'}>
-      {children}
-    </KeyboardAvoidingView>
-  </TouchableWithoutFeedback>
-);
+// const DismissKeyboardView: React.FC<{ style?: StyleProp<ViewStyle> }> = ({ children, ...props }) => (
+//   <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+//     <KeyboardAvoidingView
+//       {...props}
+//       style={[props.style, Platform.OS === 'ios' && { flex: 1 }]}
+//       behavior={Platform.OS === 'ios' ? 'padding' : 'position'}>
+//       {children}
+//     </KeyboardAvoidingView>
+//   </TouchableWithoutFeedback>
+// );
 
 export default DismissKeyboardView;
