1cbd5c0e10f642db6e794cb2b902c58c0ff1e28b
wonhopark89-dev <wonhopark89.dev@gmail.com>
2022-03-17 00:12:11 +0900

    useCallback 으로 flatList 항목 응용

--- FoodDeliveryApp/src/pages/Orders.tsx
+++ FoodDeliveryApp/src/pages/Orders.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useCallback } from 'react';
+import { FlatList, Text, View } from 'react-native';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store/reducer';
+import { Order } from '../slices/order';
+import EachOrder from '../components/EachOrder';
 
 function Orders() {
+  const orders = useSelector((state: RootState) => state.order.orders);
+  const renderItem = useCallback(({ item }: { item: Order }) => {
+    return <EachOrder item={item} />;
+  }, []);
+
   return (
     <View>
-      <Text>주문 화면</Text>
+      <FlatList data={orders} keyExtractor={(item) => item.orderId} renderItem={renderItem} />
     </View>
   );
 }
