0b97256430e8d2473969695ebd2b90acd3354f3a
wonhopark89-dev <wonhopark89.dev@gmail.com>
2022-08-02 01:17:59 +0900

    add gradient component from skia

--- animated_3d_card/components/BackgroundGradient.tsx
+++ animated_3d_card/components/BackgroundGradient.tsx
@@ -1,17 +1,51 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
+import {
+  BlurMask,
+  Canvas,
+  RoundedRect,
+  SweepGradient,
+  vec,
+} from '@shopify/react-native-skia';
 
 type BackgroundGradientProps = {
   width: number;
   height: number;
 };
 
-const BackgroundGradient: React.FC<BackgroundGradientProps> = React.memo(() => {
-  return <View style={styles.container} />;
-});
+const BackgroundGradient: React.FC<BackgroundGradientProps> = React.memo(
+  ({width, height}) => {
+    const canvasPadding = 40;
 
-const styles = StyleSheet.create({
-  container: {},
-});
+    return (
+      <Canvas
+        style={{width: width + canvasPadding, height: height + canvasPadding}}>
+        <RoundedRect
+          x={canvasPadding / 2}
+          y={canvasPadding / 2}
+          width={width}
+          height={height}
+          color={'white'}
+          r={20}>
+          <SweepGradient
+            c={vec((width + canvasPadding) / 2, (height + canvasPadding) / 2)}
+            colors={[
+              'red',
+              'orange',
+              'yellow',
+              'lightgreen',
+              'lightblue',
+              'navy',
+              'purple',
+              'pink',
+              'red', // 자연스러움 ( 시작과 동일한 색상 )
+            ]}
+          />
+          <BlurMask blur={10} style={'solid'} />
+        </RoundedRect>
+      </Canvas>
+    );
+  },
+);
 
 export {BackgroundGradient};
