63049da65263b2e7a102f8eb98a1202f084dbebb
whpark <whpark@dozn.co.kr>
2022-08-09 18:15:41 +0900

    List Animations

--- animated_3d_card/App.tsx
+++ animated_3d_card/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import Animated3DCard from './src/Animated3DCard';
+import ListAnimations from './src/ListAnimations';
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Animated3DCard />
+      {/*<Animated3DCard />*/}
+      <ListAnimations />
     </View>
   );
 };
