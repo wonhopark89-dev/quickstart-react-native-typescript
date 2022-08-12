a3b3722b2ed2ef6f635c4b4a90c96e79bf6f6ab7
whpark <whpark@dozn.co.kr>
2022-08-12 16:33:19 +0900

    chart with skia, sample

--- animated_3d_card/App.tsx
+++ animated_3d_card/App.tsx
@@ -3,12 +3,14 @@ import {StyleSheet, View} from 'react-native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import Animated3DCard from './src/Animated3DCard';
 import ListAnimations from './src/ListAnimations';
+import {Wallet} from './src/Wallet';
 
 const App = () => {
   return (
     <View style={styles.container}>
       {/*<Animated3DCard />*/}
-      <ListAnimations />
+      {/*<ListAnimations />*/}
+      <Wallet />
     </View>
   );
 };
